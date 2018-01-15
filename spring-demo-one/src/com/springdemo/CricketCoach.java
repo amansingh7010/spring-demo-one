65e9c22d41dfe3b8bdadb6be6f240a11ec849cb7
SilentAssassin <SilentAssassin@Silent>
2018-01-15 09:08:59 +0530

    First Commit

---
+++ spring-demo-one/src/com/springdemo/CricketCoach.java
@@ -0,0 +1,59 @@
+package com.springdemo;
+
+public class CricketCoach implements Coach {
+	
+	private FortuneService fortuneService;
+	private String email;
+	private String name;
+	
+	
+	
+	public CricketCoach(){
+		System.out.println("CricketCoach: inside no-arg constructor");
+	}
+	
+	
+	
+	public void setEmail(String email) {
+		System.out.println("CricketCoach: inside setter method setEmail()");
+		this.email = email;
+	}
+
+
+
+	public void setName(String name) {
+		System.out.println("CricketCoach: inside setter method setName()");
+		this.name = name;
+	}
+
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setFortuneService(FortuneService fortuneService) {
+		System.out.println("CricketCoach: inside setter method setFortuneService()");
+		this.fortuneService = fortuneService;
+	}
+
+
+	@Override
+	public String getDailyWorkout() {
+		return "Practice fast bowling for 15 minutes";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
+	}
+
+}
